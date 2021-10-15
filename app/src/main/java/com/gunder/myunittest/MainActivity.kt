e62858652ca9862cefd3a0a53c2df2615d568821
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-15 10:45:57 +0700

    kode untuk kelas MainActivity.kt

--- app/src/main/java/com/gunder/myunittest/MainActivity.kt
+++ app/src/main/java/com/gunder/myunittest/MainActivity.kt
@@ -2,10 +2,31 @@ package com.gunder.myunittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.gunder.myunittest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    //    binding
+    private lateinit var binding: ActivityMainBinding
+
+    //    viewModel
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        btnListener
+        viewModel = MainViewModel(CuboidModel())\
+        binding.btnSave.setOnClickListener(this)
+        binding.btnCalculateSurfaceArea.setOnClickListener(this)
+        binding.btnCalculateCircumference.setOnClickListener(this)
+        binding.btnCalculateVolume.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
--- app/src/main/java/com/gunder/myunittest/MainViewModel.kt
+++ app/src/main/java/com/gunder/myunittest/MainViewModel.kt
@@ -1,4 +1,13 @@
 package com.gunder.myunittest
 
-class MainViewModel {
+class MainViewModel(private val cuboidModel: CuboidModel) {
+    fun getCircumFerence() = cuboidModel.getCircumFerence()
+
+    fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+
+    fun  getVolume() = cuboidModel.getVolume()
+
+    fun  save(w: Double, l:Double, h: Double){
+        cuboidModel.save(w,l,h)
+    }
 }
